368d5d3538401633bdecd881f3a2aaadb8d24dc1
julien <dubreuil.julien@gmail.com>
2017-02-01 21:39:42 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: MU-go-rest-api
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "github.com/julienschmidt/httprouter"
+    "net/http"
+    "log"
+    "os"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    router := httprouter.New()
+    router.GET("/", Index)
+    router.GET("/hello/:name", Hello)
+
+    log.Fatal(http.ListenAndServe(":"+port, router))
+}
